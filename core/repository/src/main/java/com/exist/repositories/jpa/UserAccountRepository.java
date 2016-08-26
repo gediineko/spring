ac2336bccae22940ad8266c0459e8662131f816c
Geraldine Golong <gediineko14@gmail.com>
2016-08-26 15:43:31 +0800

    Configured user account repo & service implemenatation

--- core/repository/src/main/java/com/exist/repositories/jpa/UserAccountRepository.java
+++ core/repository/src/main/java/com/exist/repositories/jpa/UserAccountRepository.java
@@ -1,10 +1,19 @@
 package com.exist.repositories.jpa;
 
 import com.exist.model.entities.UserAccount;
+import com.exist.model.ref.RoleType;
 import com.exist.repositories.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserAccountRepository extends BaseRepository<UserAccount, Long> {
-    UserAccount findByUsername(String s);
+    Optional<UserAccount> findByUsername(String s);
+
+    @Query("select distinct u from UserAccount u inner join u.roles r where r.roleType = :roleType")
+    List<UserAccount> findByRoleType(@Param("roleType") RoleType roleType);
 }
--- core/services/src/main/java/com/exist/services/UserAccountService.java
+++ core/services/src/main/java/com/exist/services/UserAccountService.java
@@ -1,6 +1,17 @@
 package com.exist.services;
 
+import com.exist.model.dto.UserAccountDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserAccountService extends UserDetailsService {
+
+    UserAccountDto get(Long id);
+
+    List<UserAccountDto> getAll();
+
+    UserAccountDto create(UserAccountDto userAccountDto);
+
+    UserAccountDto update(UserAccountDto userAccountDto);
 }
