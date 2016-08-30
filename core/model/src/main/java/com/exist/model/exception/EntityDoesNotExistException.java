740632d5cff1ca6afafdfa5a0f21c14353709cd4
Geraldine Golong <gediineko14@gmail.com>
2016-08-30 14:11:58 +0800

    Created exception models

--- core/model/src/main/java/com/exist/model/exception/EntityDoesNotExistException.java
+++ core/model/src/main/java/com/exist/model/exception/EntityDoesNotExistException.java
@@ -3,29 +3,11 @@ package com.exist.model.exception;
 /**
  * Created by jvillanueva on 8/29/16.
  */
-public class EntityDoesNotExistException extends Exception {
+public class EntityDoesNotExistException extends BaseException {
 
     private static final long serialVersionUID = -4202518367237468224L;
 
-    private String messageCode;
-    private Object[] args;
-
-    public EntityDoesNotExistException(String message, String messageCode) {
-        super(message);
-        this.messageCode = messageCode;
-    }
-
     public EntityDoesNotExistException(String message, String messageCode, Object[] args) {
-        super(message);
-        this.messageCode = messageCode;
-        this.args = args;
-    }
-
-    public String getMessageCode() {
-        return messageCode;
-    }
-
-    public Object[] getArgs() {
-        return args;
+        super(message, messageCode, args);
     }
 }
---
+++ core/model/src/main/java/com/exist/model/exception/InvalidFileTypeException.java
@@ -0,0 +1,13 @@
+package com.exist.model.exception;
+
+/**
+ * Created by ggolong on 8/30/16.
+ */
+public class InvalidFileTypeException extends BaseException {
+
+    private static final long serialVersionUID = -5889396907316870353L;
+
+    public InvalidFileTypeException(String message, String messageCode, Object[] args) {
+        super(message, messageCode, args);
+    }
+}
